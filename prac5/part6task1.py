f8080a6c47c4352b2fd69939152c0dbfb50c671c
Timur <MuratovTim3@gmail.com>
2023-04-20 23:59:27 +0300

    getting all game states

--- prac5/part6task1.py
+++ prac5/part6task1.py
@@ -1,8 +1,15 @@
 import part6game as source
+import itertools
 import graphviz
 
 def make_model(game, start_state):
-
+    locations = game.keys()
+    events = [1, 2]
+    for room in game:
+        if 'toggle' in game[room]:
+            events.append(room + "toggle")
+    for i in itertools.product(locations, events):
+        print(i)
 
 
 make_model(source.game, source.START_STATE)
