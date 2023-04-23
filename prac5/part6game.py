387b990a25067ea5661ff130e81955b9a862a16c
Timur <MuratovTim3@gmail.com>
2023-04-23 17:40:07 +0300

    Game source for 6.1-6.4

--- prac5/part6game.py
+++ prac5/part6game.py
@@ -1,7 +1,35 @@
+from typing import NamedTuple
+
+
+class State(NamedTuple):
+    room: str
+    toggle1: int = 0
+    toggle2: int = 0
+
+
 # Функция перехода из комнаты в комнату
 def go(room):
     def func(state):
-        return dict(state, room=room)
+        new_state = list(state)
+        new_state[0] = room
+        return State(*new_state)
+
+    return func
+
+
+# Функция действия в комнате
+def toggle(ind):
+    def func(state):
+        new_state = list(state)
+        new_state[ind] = (state[ind] + 1) % 2
+
+        for effect in toggles[ind]:
+            if effect[1] in game[effect[0]]:
+                del game[effect[0]][effect[1]]
+            else:
+                game[effect[0]][effect[1]] = effect[2]
+
+        return State(*new_state)
 
     return func
 
@@ -33,19 +61,30 @@ game = {
     )
 }
 
+# Функции рычагов. Добавляются к структуре игры при активации
+toggles = {
+    1: [['room3', 'left', go('room0')]],
+    2: [['room3', 'left', go('room0')],
+        ['room5', 'right', go('room2')],
+        ['room5', 'down', go('room1')],
+        ['room4', 'up', go('room2')],
+        ['room4', 'left', go('room0')]]
+}
+
 # Стартовое состояние
-START_STATE = dict(room='room0')
+START_STATE = State(room='room0')
+GOAL = 'room2'
 
 
 def is_goal_state(state):
     """
     Проверить, является ли состояние целевым.
     """
-    return state['room'] == 'room2'
+    return state[0] == GOAL
 
 
 def get_current_room(state):
     """
     Выдать комнату, в которой находится игрок.
     """
-    return state['room']
+    return state[0]
