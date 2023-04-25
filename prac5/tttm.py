17762a446f9d1499ddf743baf8a4281223ae4da3
Timur <MuratovTim3@gmail.com>
2023-04-25 22:22:37 +0300

    Game mechanics overhaul

--- prac5/tttm.py
+++ prac5/tttm.py
@@ -1,27 +1,51 @@
-# Функция перехода из комнаты в комнату
+from typing import NamedTuple
+
+
+class State(NamedTuple):
+    player: str
+    alice: str
+    bob: str
+    red_key: str
+    blue_key: str
+    green_key: str
+
+
+# Функция перехода из комнаты в комнату.
+# Возвращает лист НОВЫХ состояний, т.е. отбрасывает варианты,
+# ведущие в текущее состояние (переходы, не имея нужного ключа)
 def go(room, req):
     def func(state):
-        new_state = state.copy()
-        active_player = players[state['player']]
-        if req in active_player['inventory']:
-            new_state[active_player + '_room'] = room
-        return new_state
+        # Персонаж не уйдет из своей комнаты
+        if state[0] == 'alice' and state[1] == 'red room':
+            return []
+        if state[0] == 'bob' and state[1] == 'blue room':
+            return []
+
+        states = []
+        new_state = list(state)
+        player_ind = ['alice', 'bob'].index(state[0]) + 1
+        req_ind = ['red_key', 'blue_key', 'green_key'].index(req) + 3
+        if new_state[player_ind] == new_state[req_ind]:
+            new_state[player_ind] = room
+            new_state[req_ind] = room
+            states.append(State(*new_state))
+            for i in range(1, 3):
+                if new_state[((req_ind + i) % 3) + 3] == state[player_ind]:
+                    new_state[((req_ind + i) % 3) + 3] = room
+                    states.append(State(*new_state))
+
+        return states
 
     return func
 
 
-def pick():
-    def func(state, key):
-        if key not in players[state['player']]['inventory']:
-            players[state['player']]['inventory'].append(key)
-
-    return func
-
-
-def drop():
-    def func(state, key):
-        if key in players[state['player']]['inventory']:
-            players[state['player']]['inventory'].remove(key)
+def switch_player():
+    def func(state):
+        new_state = list(state)
+        players = ['alice', 'bob']
+        ind = (players.index(state[0]) + 1) % 2
+        new_state[0] = players[ind]
+        return [State(*new_state)]
 
     return func
 
@@ -30,47 +54,37 @@ def drop():
 game = {
     'east room': dict(
         up=go('blue room', 'blue_key'),
-        left=go('west room', 'green_key')
+        left=go('west room', 'green_key'),
+        switch=switch_player()
     ),
     'west room': dict(
         up=go('red room', 'red_key'),
-        right=go('east room', 'green_key')
+        right=go('east room', 'green_key'),
+        switch=switch_player()
     ),
-    'red_room': dict(
-        down=go('west room', 'red_key')
+    'red room': dict(
+        down=go('west room', 'red_key'),
+        switch=switch_player()
     ),
-    'room3': dict(
-        down=go('east room', 'blue_key')
-    )
-}
-
-players = {
-    'alice': dict(
-        inventory=[],
-        pick=pick(),
-        drop=drop()
-    ),
-    'bob': dict(
-        inventory=[],
-        pick=pick(),
-        drop=drop()
+    'blue room': dict(
+        down=go('east room', 'blue_key'),
+        switch=switch_player()
     )
 }
 
 # Стартовое состояние
-START_STATE = dict(
+START_STATE = State(
     player='alice',
-    alice_room='west room',
-    bob_room='east room',
+    alice='west room',
+    bob='east room',
     red_key='east room',
     blue_key='west room',
     green_key='east room'
 )
-GOAL = 'room2'
 
 
 def is_goal_state(state):
     """
     Проверить, является ли состояние целевым.
     """
-    return state['alice_room'] == 'red room' and state['bob_room'] == 'blue room'
+    return state.alice == 'red room' and state.bob == 'blue room'
